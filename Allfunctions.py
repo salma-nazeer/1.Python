2be098d7e3568c6729a2138563ea5986168218d2
Salma <salmanajmu21@gmail.com>
2025-08-06 15:18:29 +0530

    Add files via upload

---
+++ Allfunctions.py
@@ -0,0 +1,38 @@
+class allfunctions():
+    def subfields():
+        for subfields in SubfieldsInAI:
+            print("Subfields In AI are:")
+            print(subfields,end="")
+            return SubfieldsinAI
+    def oddeven():
+        num=int(input("Enter a number:"))
+        if((num%2)==0):
+            print(num, "is Even number")
+        else:
+            print(num, "is odd number")
+            return oddeven
+    def elegibility():
+        if(male<=21):
+            print("not eligible")
+            result="not eligible"
+        else:
+            print("elegible")
+            result="elegible"
+            return result
+    def percentage():
+        subject1=int(input("subject1:"))
+        subject2=int(input("subject2:"))
+        subject3=int(input("subject3:"))
+        subject4=int(input("subject4:"))
+        subject5=int(input("subject5:"))
+        total = Subject1 + Subject2 + Subject3 + Subject4 + Subject5
+        print("Total:",total)
+        percentage = (total / 500) * 100
+        print("Percentage:", percentage)
+        return percentage
+    def triangle():
+            print("Area of Triangle:", area_formula)
+            print("Perimeter of Triangle:",perimeter_formula)
+            return triangle
+  
+         
